7d778fcf76f6573fdd0184233115518a8d07d9cb
Marco García 1086663 <marcoagarcia@uade.edu.ar>
2025-02-20 19:08:12 -0300

    Last check of all exercises. Added model & util packages to organize interfaces, implementations & other methods.

--- src/main/java/org/example/ejercicio04/model/RepeatedSet.java
+++ src/main/java/org/example/ejercicio04/model/RepeatedSet.java
@@ -1,7 +1,7 @@
 package org.example.ejercicio04.model;
 
 public interface RepeatedSet {
-    // Métodos adicionales específicos para conjuntos con repetición
+
     void add(int element, int quantity);
     void remove(int element, int quantity);
     int getCount(int element);
